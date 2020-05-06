06a9bf2a082ce2f295dd0aec74aa0359a55453c6
cheeseBG <51084152+JangJungIk@users.noreply.github.com>
2020-05-06 19:25:13 +0900

    설정 추가

---
+++ app/src/main/java/com/example/teamtotest/activity/MyInfoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.teamtotest.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.teamtotest.R
+
+class MyInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info)
+    }
+
+
+}
--- app/src/main/java/com/example/teamtotest/fragment/Frag4.kt
+++ app/src/main/java/com/example/teamtotest/fragment/Frag4.kt
@@ -1,12 +1,18 @@
 package com.example.teamtotest.fragment
 
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.teamtotest.R
+import com.example.teamtotest.activity.AnnounceActivity
+import com.example.teamtotest.activity.MyInfoActivity
+import com.example.teamtotest.activity.NavigationbarActivity
+import kotlinx.android.synthetic.main.bottombar_fragment4.view.*
 
 class Frag4 : Fragment (){
 
@@ -19,8 +25,16 @@ class Frag4 : Fragment (){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //val view = inflater.inflate(R.layout.bottombar_fragment1, null)
-        return inflater.inflate(R.layout.bottombar_fragment4, null)
+        val view = inflater.inflate(R.layout.bottombar_fragment4, null)
+
+        view.setting_myInfo.setOnClickListener {
+            startActivity(Intent(requireActivity(), MyInfoActivity::class.java))
+        }
+
+        view.setting_announce.setOnClickListener {
+            startActivity(Intent(requireActivity(), AnnounceActivity::class.java))
+        }
+
+        return view
     }
-  var adn = 3
 }
