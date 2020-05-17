55092b105357b4a9183ce13b387201f85d1da746
cheeseBG <51084152+JangJungIk@users.noreply.github.com>
2020-05-17 15:54:44 +0900

    알림 UI 수정 및 알림표시 Activity 추가
    알림 UI 수정 및 알림표시 Activity 추가

--- app/src/main/java/com/example/teamtotest/activity/AlertDisplayActivity.kt
+++ app/src/main/java/com/example/teamtotest/activity/AlertDisplayActivity.kt
@@ -1,26 +1,20 @@
 package com.example.teamtotest.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import com.example.teamtotest.R
 import kotlinx.android.synthetic.main.activity_alert.*
+import kotlinx.android.synthetic.main.activity_alert_display.*
 
-class AlertActivity : AppCompatActivity() {
+class AlertDisplayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alert)
+        setContentView(R.layout.activity_alert_display)
 
-        setSupportActionBar(alert_toolbar)
+        setSupportActionBar(alert_display_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 만들기
-
-        alert_display.setOnClickListener {
-            intent = Intent(this, AlertDisplayActivity::class.java)
-            startActivity(intent)
-        }
     }
 
     // 뒤로가기 버튼 활성화
