b734ad752a2801cfa642a233f730b0ea58d3566f
Yang, heerim <qkq1002@naver.com>
2020-05-20 19:22:09 +0900

    채팅방 수정
    채팅메세지 길 때 짤리는 현상 수정,
     채팅방 내에서 바로 파일 올리기 기능 추가

--- app/src/main/java/com/example/teamtotest/activity/ChatActivity.kt
+++ app/src/main/java/com/example/teamtotest/activity/ChatActivity.kt
@@ -1,22 +1,29 @@
 package com.example.teamtotest.activity
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.example.teamtotest.Push
 import com.example.teamtotest.R
 import com.example.teamtotest.adapter.ChatListAdapter
+import com.example.teamtotest.dto.FileDTO
 import com.example.teamtotest.dto.MembersDTO
 import com.example.teamtotest.dto.MessageDTO
 import com.example.teamtotest.dto.UserDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_chat.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -34,6 +41,7 @@ class ChatActivity : AppCompatActivity() {
     private var projectName : String? = null
     private var howManyMembers : String? = null
     private var userDTOList : HashMap<String, UserDTO> ?= null
+    private lateinit var filePath : Uri
 
     private var ChatMessageList: ArrayList<HashMap<String, String>> = ArrayList<HashMap<String, String>>()
     private var ChatMessageData: HashMap<String, String> = HashMap<String, String>()
@@ -141,7 +149,13 @@ class ChatActivity : AppCompatActivity() {
                 chat_drawer.openDrawer(GravityCompat.END);
                 true
             }
-
+            R.id.toolbar_file -> {
+                val intent = Intent()
+                intent.type = "*/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                startActivityForResult(Intent.createChooser(intent, "파일을 선택하세요."), 0)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -163,6 +177,70 @@ class ChatActivity : AppCompatActivity() {
         databaseReference = firebaseDatabase!!.getReference("ProjectList").child(PID.toString()).child("members")
         databaseReference!!.removeEventListener(members_listener)
         super.onStop()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            Log.e("data", data.toString())
+            filePath = data!!.data!!
+            val fileName: String? = getFileName(filePath)
+            uploadFile(fileName)
+        }
+    }
+
+    private fun uploadFile(filename: String?)
+    { //업로드할 파일이 있으면 수행
+        if (filePath != null)
+        { //storage
+            val storage = FirebaseStorage.getInstance()
+
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            val storageRef = storage.getReferenceFromUrl("gs://teamtogether-bdfc9.appspot.com")
+
+            storageRef.child(filename!!).putFile(filePath) //성공시
+                .addOnSuccessListener { Toast.makeText(applicationContext, "업로드 완료!", Toast.LENGTH_SHORT).show() } //실패시
+                .addOnFailureListener { Toast.makeText(applicationContext, "업로드 실패!", Toast.LENGTH_SHORT).show() } //진행중
+
+        } else {
+            Toast.makeText(applicationContext, "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show()
+        }
+        uploadFileInfoToDB(filename!!)
+    }
+
+    private fun uploadFileInfoToDB(fileName : String){
+        var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        val uid : String = firebaseAuth.currentUser!!.uid
+        val userName : String = firebaseAuth.currentUser!!.displayName!!
+
+        val date_format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val date = date_format.format(System.currentTimeMillis())
+
+        val fileDTO : FileDTO = FileDTO(fileName, date, uid, userName)
+
+        databaseReference = firebaseDatabase!!.reference.child("ProjectList").child(PID.toString()).child("file").push()
+        databaseReference!!.setValue(fileDTO)
+
+    }
+
+    private fun getFileName(uri: Uri) : String? {
+        var result : String? = null
+        if (uri.scheme!!.equals("content"))
+        {
+            var cursor : Cursor? = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor?.close()
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result
     }
 
     private fun exitProject(){
