2d835acdf0f37163a60e8899e549fa8a0eed7b2c
장하영 <hyj9829@naver.com>
2020-06-29 22:34:44 +0900

    wiki

--- app/src/main/java/com/example/teamtotest/AlarmService.kt
+++ app/src/main/java/com/example/teamtotest/AlarmService.kt
@@ -10,8 +10,6 @@ import kotlinx.android.synthetic.main.activity_add_todo.*
 
 class AlarmService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.e("Service", "알람 서비스 된다!!")
-
         val PID = intent?.getStringExtra("PID")
         val todoName = intent?.getStringExtra("todo_name")
         val scheduleName = intent?.getStringExtra("schedule_name")
